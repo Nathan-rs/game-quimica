1a4ab84b8ba542e87e5f65f86efa76a880d9f787
Nathan <nata.ramos@acad.ifmaedu.br>
2024-03-04 18:12:57 -0300

    game stylization

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import '../../index.css'
 import { CardOptions } from '../CardOptions/CardOptions'
 import { useState } from 'react'
+import image from '../../assets/images/quimica-estruture.svg'
 
 export function Main() {
     const [clickedButton, setClickedButton] = useState<string[]>([])
@@ -14,15 +15,19 @@ export function Main() {
         <>
             <section className="container default-view">
                 <div className="content">
-                    <div className="image">
-                        <img src="" alt="" />
+                    <div className="idCadeia">
+                        <span>Prefixo</span>
                     </div>
-                    <div className="message">
+                    <div className="image">
+                        <img src={image} alt="" style={{width:'300px', height: '200px', padding: '10px'}} />
+                    </div>
+                    <div className="completeCadeia">
                         {
                             clickedButton.map((value, index) => (
                                 <button
                                 key={index}
-                                onClick={() => handleRemoveButton(index)}>{value}</button>
+                                onClick={() => handleRemoveButton(index)}
+                                className="btnMain">{value}</button>
                             ))
                         }
                     </div>
