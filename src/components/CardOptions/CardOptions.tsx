c9c86f9fbf4ca316462c9c7a3abfbe41556e59d6
Nathan <nata.ramos@acad.ifma.edu.br>
2024-03-04 14:08:18 -0300

    created list switcher

--- src/components/CardOptions/CardOptions.tsx
+++ src/components/CardOptions/CardOptions.tsx
@@ -1,4 +1,5 @@
-import { Prefixos } from "../../functions/Nomeclatuas" 
+import { useState } from "react";
+import { Prefixos, Infixos, Sufixos } from "../../functions/Nomeclatuas" 
 
 interface CardOptionsProps{
     onButtonClick: (text: string) => void;
@@ -6,17 +7,53 @@ interface CardOptionsProps{
 
 export function CardOptions({ onButtonClick }: CardOptionsProps) {
 
-    const listPrefixos: string[] = Object.values(Prefixos)
+    // const [listButton, SetListButton] = useState(true)
+    const [listType, setListType] = useState<"prefixos" | "infixos" | "sufixos">("prefixos");
+    // const listPrefixos: string[] = Object.values(Prefixos)
+    // const listInfixos: string[] = Object.values(Infixos)
 
-    function handleClick(text: string) {
+    // const buttons: string[] = listButton ? Object.values(Prefixos) : Object.values(Infixos)
+
+    const handleClick = (text: string) => {
         onButtonClick(text);
+        handleToggleList();
+    }
+
+    const handleToggleList = () => {
+        setListType((prevType) => {
+            switch (prevType) {
+                case "prefixos":
+                    return "infixos";
+                case "infixos":
+                    return "sufixos";
+                case "sufixos":
+                    return "prefixos";
+                default:
+                    return "prefixos";
+            }
+        });
+    };
+
+    let listButtons: string[] = [];
+    switch (listType) {
+        case "prefixos":
+            listButtons = Object.values(Prefixos);
+            break;
+        case "infixos":
+            listButtons = Object.values(Infixos);
+            break;
+        case "sufixos":
+            listButtons = Object.values(Sufixos);
+            break;
+        default:
+            listButtons = Object.values(Prefixos);
     }
 
     return (
         <footer className="default-view">
             <div className="button-options">
                 {
-                    listPrefixos.map((text, index) => (
+                    listButtons.map((text, index) => (
                         <button
                         onClick={() => handleClick(text)}
                         className="btn"
--- src/functions/Nomeclatuas.ts
+++ src/functions/Nomeclatuas.ts
@@ -9,3 +9,20 @@ export enum Prefixos {
     Hept = "Hept",
     Oct = "Oct"
 }
+
+export enum Infixos {
+    An = "an",
+    En = "en",
+    Yn = "yn"
+}
+
+export enum Sufixos {
+    Ano = "ano",
+    Eno = "eno",
+    Ino = "ino",
+    Il = "il",
+    Al = "al",
+    Ol = "ol",
+    Ona = "ona",
+    Oico = "oico",
+}
